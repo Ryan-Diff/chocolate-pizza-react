7b8d9aa2fbee576cd98835c770e7a1208ad29f90
ryan diffenbaugh <ryandiffenbaugh@gmail.comgit config --global user. name Ryan-Diffgit config --global user.email ryandiffenbaugh@gmail.com>
2020-08-03 15:26:29 -0700

    added assets

---
+++ header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import fb-icon from './fb-icon/png'
+
+
+class HeaderItem extends React.Component {
+    render() {
+        return (
+        <div>
+            <div id="top-logo">
+                <img src='./assets/logo.png' id="logo"/> 
+                <label>
+                    <h1 class="logo">Delicious</h1>
+                    <p class="logo">THE BEST FOOD BLOG ON THE WEB.</p>
+                </label>
+            </div>
+
+            <div id='social-icons'>
+
+            </div>
+            );
+
+        </div>    
+        )
+        } 
+    }
