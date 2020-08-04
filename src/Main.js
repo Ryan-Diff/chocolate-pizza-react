4b91220ccba409496485ff86abfdbcaa2836b692
Michelle Stermitz <stermitzm@gmail.com>
2020-08-03 17:49:53 -0700

    Added Notepad component

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Notepad from 'Notepad.js';
 
 class Main extends React.Component {
     render() {
@@ -22,6 +23,7 @@ class Main extends React.Component {
                     Using an ice cream machine, pour liquids into the frozen basin and process according to manufacturer instructions, i,e., let spin and thicken for 20 minutes before adding marscarpone, fig jam mixture, and the nuts. Continue to process for +/- 10 minutes. Pour semi-frozen mixture into a pyrex dish or glass tupperware. Freeze for at least two hours before serving. 
                 </p>
             </div>
+            <Notepad notepad='list-bg.png'/>
         
 
       </main>
@@ -31,30 +33,6 @@ class Main extends React.Component {
 }
 
 export default Main
-
-
-
-
-
-            // <div id='note-pad'>
-            //     <img src='./assets/list-bg.png'>
-            //     <ul>
-            //         <li>1 1/2 cups milk</li>
-            //         <li>1/2 cup marscarpone</li>
-            //         <li>1/2 tsp pink salt</li>
-            //         <li>1 lb Black Mission Figs</li>
-            //         <li>1/2 cup cup brown sugar</li>
-            //         <li>2-4 tbsp water</li>
-            //         <li>1 1/2 cups heavy cream</li>
-            //         <li>1/3 granulated </li>
-            //         <li>2 egg yolks</li>
-            //         <li>1 lemon, juiced</li>
-            //         <li>2 tbsp butter</li>
-            //         <li>1 cup honey roasted pecans, roughly chopped</li>
-            //     </ul>
-            //     </img>
-                
-            // </div>
 
 
 
